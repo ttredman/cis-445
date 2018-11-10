357b40b095cda9b05111356fc4796dfdd313072c
Tyler? <tredman@una.edu>
2018-11-10 00:19:55 +0000

    fixed avg review info for customer by category

--- server.js
+++ server.js
@@ -6,8 +6,6 @@
 var http = require('http');
 var path = require('path');
 
-var async = require('async');
-var socketio = require('socket.io');
 var express = require('express');
 const post_test = require('./test/test1.json');
 const put_test = require('./test/test2.json');
@@ -15,75 +13,11 @@ const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://omega.unasec.info/amazon";
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
+
 var router = express();
 var server = http.createServer(router);
-var io = socketio.listen(server);
 
 router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
-
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-// TODO: get middleware body parser for Express
 
 // Get a review
 router.get('/review/:reviewid', jsonParser, function (req, res) {          
@@ -253,7 +187,7 @@ router.get('/review/info/:custid', jsonParser, function (req, res) {
         { 
             $group: 
             { 
-                _id: null, 
+                _id: "$product.category", 
                 avgStars: { $avg : "$review.star_rating" }, 
                 avgHelpfulVotes: { $avg : "$votes.helpful_votes" },
                 avgTotalVotes: { $avg : "$votes.total_votes" }
