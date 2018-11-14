2bfaabd154bf27924b92bb67bbdba677089d0ff7
Tyler? <tredman@una.edu>
2018-11-14 01:57:41 +0000

    restructured mongo connection

--- server.js
+++ server.js
@@ -2,13 +2,23 @@
     Tyler Redman
     CS 445: Advanced Database
     REST API Final Project
+    
+    API Paths:
+      Get a review - GET /review/:reviewid
+      Get random reviews by stars - GET /review/random/:n/:stars
+      Get random reviews by date - GET /review/random/:n/:from_date/:to_date
+      Add a review - POST /review/:reviewid
+      Update a review - PUT /review/:reviewid
+      Delete a review - DELETE /review/:reviewid
+      
+      Get an average of review stars over time - GET /review/:from/:to
+      Get an average of helpful votes by product - GET /review/helpful/:prodid
+      Get average review info for a customer by category - GET /review/info/:custid
 */
 var http = require('http');
 var path = require('path');
 
 var express = require('express');
-const post_test = require('./test/test1.json');
-const put_test = require('./test/test2.json');
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 const MongoClient = require('mongodb').MongoClient;
@@ -19,167 +29,204 @@ var server = http.createServer(router);
 
 router.use(express.static(path.resolve(__dirname, 'client')));
 
-// Get a review
-router.get('/review/:reviewid', jsonParser, function (req, res) {          
-
-    MongoClient.connect(url,  { useNewUrlParser: true }, function(err, db) {
+MongoClient.connect(url,  { useNewUrlParser: true }, function(err, db) {
     if (err) throw err;
     var dbo = db.db("amazon");  
     var collection = dbo.collection('reviews');
-
-    collection.aggregate([{$limit: 100}, {$unwind: "$review"}, {$match: {"review.id": `${req.params.reviewid}`}}]).toArray(function(err, results) { 
-      // callback arguments are err or an array of results
-    if(!err) {
-
-        console.log(results.length);
-
-        for(var i = 0; i < results.length; i++) {
-
-          console.log(results[i]);
-        }
-
-        res.json(results);
-    }
-
-    else {
-        res.send(err);
-       db.close();
-    }
-     });
-    });
-});
-
-// Post a review
-router.post('/review/:reviewid', function(req, res) {
-  res.send(post_test);
-});
-
-// Update a review
-router.put('/review/:reviewid', function(req, res) {
-  res.send(put_test);
-});
-
-// Delete a review
-router.delete('/review/:reviewid', jsonParser, function (req, res) {
-  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("amazon");  
-      var collection = dbo.collection('reviews');
-      var myquery = { "review.id" : `${req.params.reviewid}` };
-      
-      collection.deleteMany(myquery, function(err, obj) {
-        if (err) throw err;
-        console.log("1 document deleted");
-        db.close();
-      });
-  });
-});
-/*
-// Get random reviews by stars
-router.get('/review/:n/:stars', function(req, res) {
-  res.send("These are reviews with " + req.params.stars + " stars.");
-});
-
-// Get random reviews by date
-router.get('/review/:n/:from_date/:to_date', function(req, res) {
-  res.send("These are reviews from " + req.params.from_date + " to " + req.params.to_date);
-});
-
-
-router.get('/review/helpful/:prodid', jsonParser, function (req, res) {          
-
-    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("amazon");  
-      var collection = dbo.collection('reviews');
+    
+  // Get a review
+  router.get('/review/:reviewid', jsonParser, function (req, res) {          
   
-      collection.aggregate([ 
-        { $limit : 1000000 }, 
-        { $match : { "product.id" : `${req.params.prodid}` }}, 
-        { 
-          $group: 
-            { 
-              _id: null, 
-              avgHelpfulVotes: { $avg : "$votes.helpful_votes" } 
-              
-            } 
-          
-        }
+      collection.aggregate(
+        [
+          { $limit: 100 }, 
+          { $unwind: "$review" }, 
+          { $match: {"review.id": `${req.params.reviewid}`} }
         ]).toArray(function(err, results) { 
         // callback arguments are err or an array of results
-      if(!err) {
+            if(!err) {
+        
+                console.log(results.length);
+        
+                for(var i = 0; i < results.length; i++) {
+                  console.log(results[i]);
+                }
+        
+                res.json(results);
+            }
+        
+            else {
+                res.send(err);
+                db.close();
+            }
+          });
+  });
   
-          console.log(results.length);
+  // Post a review
+  router.post('/review', jsonParser, function (req, res) {
+    if (!req.body) return res.sendStatus(400);
   
-          for(var i = 0; i < results.length; i++) {
+    collection.insertOne(req.body, function(err, results) {
+        if (err) throw err;
+        res.json(results);
+        db.close();
+    });
+  });  
   
-            console.log(results[i]);
-          }
-  
-          res.json(results);
-      }
-  
-      else {
-          res.send(err);
-          db.close();
-      }
+  // Update a review
+  router.put('/review/:reviewid', jsonParser, function (req, res) {
+    var filter = { "review.id" : req.params.reviewid };
+    var updates = { $set: req.body};
+    
+    collection.updateMany(filter, updates, {multi: false}, function(err, results) {
+        if (err) throw err;
+        res.send(results);
+        db.close();
     });
   });
-});
-
-router.get('/review/:from/:to', jsonParser, function (req, res) {          
-
-    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("amazon");  
-      var collection = dbo.collection('reviews');
-      var from = new Date(`${req.params.from}`);
-      var to = new Date(`${req.params.to}`);
   
-      collection.aggregate([
-        { $limit : 1000000 },
-        {
-            $match:
-                { "review.date" : { $gte : from, $lte : to } } 
-        }, 
-        { 
-            $group: 
+  // Delete a review
+  router.delete('/review/:reviewid', jsonParser, function (req, res) {
+    var myquery = { "review.id" : `${req.params.reviewid}` };
+    
+    collection.deleteMany(myquery, function(err, obj) {
+      if (err) throw err;
+      console.log("1 document deleted");
+      db.close();
+    });
+  });
+
+  // Get random reviews by stars
+  router.get('/review/random/:n/:stars', function (req, res) {      
+
+    var stars = parseInt(req.params.stars);
+    var review_num = parseInt(req.params.n);
+
+    collection.aggregate(
+      [
+        { $match: {"review.star_rating": stars } }, 
+        { $sample: { size: review_num } }
+      ]).toArray(function(err, results) { 
+        if(!err) {
+            res.json(results);
+        }
+        else {
+            res.send(err);
+            db.close();
+        }
+      });
+  });
+  
+  // Get random reviews by date
+  router.get('/review/random/:n/:from_date/:to_date', function (req, res) {      
+ 
+        if (err) throw err;
+        var from = new Date(req.params.from_date);
+        var to = new Date(req.params.to_date);
+        var review_num = parseInt(req.params.n);
+
+        collection.aggregate(
+          [
             { 
-                _id: null, 
-                avgStars: { $avg : "$review.star_rating" } 
-            } 
-        } 
-]).toArray(function(err, results) { 
-        // callback arguments are err or an array of results
-      if(!err) {
-  
-          console.log(results.length);
-  
-          for(var i = 0; i < results.length; i++) {
-  
-            console.log(results[i]);
-          }
-  
-          res.json(results);
-      }
-  
-      else {
-          res.send(err);
-          db.close();
-      }
-    });
+              $match: {
+                        $and: [
+                                { "review.date": {$gte : from} }, 
+                                { "review.date": {$lte: to} }
+                              ] 
+                      }
+            }, 
+            { $sample: { size: review_num } }
+          ]).toArray(function(err, results) { 
+              if(!err) {
+                  res.json(results);
+              }
+              else {
+                  res.send(err);
+                  db.close();
+              }
+            });
   });
-});
-*/
+  
+  // average of helpful votes by product
+  router.get('/review/helpful/:prodid', jsonParser, function (req, res) {          
+    
+        collection.aggregate(
+          [ 
+            { $limit : 1000000 }, 
+            { $match : { "product.id" : `${req.params.prodid}` }}, 
+            { 
+              $group: 
+                { 
+                  _id: null, 
+                  avgHelpfulVotes: { $avg : "$votes.helpful_votes" } 
+                } 
+              
+            }
+          ]).toArray(function(err, results) { 
+          // callback arguments are err or an array of results
+        if(!err) {
+    
+            console.log(results.length);
+    
+            for(var i = 0; i < results.length; i++) {
+    
+              console.log(results[i]);
+            }
+    
+            res.json(results);
+        }
+    
+        else {
+            res.send(err);
+            db.close();
+        }
+      });
+  });
+  
+  // average of review stars over time
+  router.get('/review/stars/:from/:to', jsonParser, function (req, res) {          
 
-router.get('/review/info/:custid', jsonParser, function (req, res) {          
-
-    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("amazon");  
-      var collection = dbo.collection('reviews');
-      
-      collection.aggregate([
+        var from = new Date(`${req.params.from}`);
+        var to = new Date(`${req.params.to}`);
+    
+        collection.aggregate(
+          [
+            { $limit : 1000000 },
+            {
+                $match:
+                    { "review.date" : { $gte : from, $lte : to } } 
+            }, 
+            { 
+                $group: 
+                { 
+                    _id: null, 
+                    avgStars: { $avg : "$review.star_rating" } 
+                } 
+            } 
+          ]).toArray(function(err, results) { 
+              // callback arguments are err or an array of results
+              if(!err) {
+                  console.log(results.length);
+          
+                  for(var i = 0; i < results.length; i++) {
+                    console.log(results[i]);
+                  }
+          
+                  res.json(results);
+              }
+          
+              else {
+                  res.send(err);
+                  db.close();
+              }
+            });
+  });
+  
+  //average review info for a customer by category
+  router.get('/review/info/:custid', jsonParser, function (req, res) {          
+        
+    collection.aggregate(
+      [
         /*{ $limit : 1000000 }, */
         {
             $match: { customer_id : `${req.params.custid}` } 
@@ -193,14 +240,12 @@ router.get('/review/info/:custid', jsonParser, function (req, res) {
                 avgTotalVotes: { $avg : "$votes.total_votes" }
             } 
         } 
-]).toArray(function(err, results) { 
-        // callback arguments are err or an array of results
+    ]).toArray(function(err, results) { 
+      // callback arguments are err or an array of results
       if(!err) {
-  
           console.log("YEP");
   
           for(var i = 0; i < results.length; i++) {
-  
             console.log(results[i]);
           }
   
@@ -213,14 +258,14 @@ router.get('/review/info/:custid', jsonParser, function (req, res) {
       }
     });
   });
+  
+  db.close();
 });
-
 
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
-
 
 
